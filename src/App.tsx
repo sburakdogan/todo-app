02a9f44b373f6eec87df113ed69e8cdce30bf10e
Burak Dogan <46339962+sburakdogan@users.noreply.github.com>
2023-04-12 14:01:05 +0300

    removed comment lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,7 @@
-import { useState } from 'react';
 import './App.css';
-import Form from 'react-bootstrap/Form';
-import { Alert, Button } from 'react-bootstrap';
 import TodoForm from './components/todo-form';
 
-// type TodoItems = {
-//   id: number;
-//   name: string;
-//   description: string;
-// };
-
 function App() {
-  // const [items, setItems] = useState<TodoItems[]>([]);
-  // const [title, setTitle] = useState('');
-  // const [description, setDescription] = useState('');
-
-  // const removeItem = (item: TodoItems) => {
-  //   const filteredItems = items.filter(x => x.id !== item.id);
-  //   setItems(filteredItems);
-  // }
-
-  // const handleSubmitForm = (event: any) => {
-  //   event.preventDefault();
-
-  //   const todoItem: TodoItems = {
-  //     id: Math.random(),
-  //     name: title,
-  //     description: description
-  //   }
-
-  //   setItems(items => [...items, todoItem]);
-  //   setTitle('');
-  //   setDescription('');
-  // }
-
   return (
     <div className="App">
       <TodoForm />
